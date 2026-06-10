f3b65c6aff769135685eb397429d58cca9616f0d
yashikagupta-ai <yashikagupta561@gmail.com>
2026-06-10 11:54:58 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,24 +2,50 @@ import { FiGithub } from 'react-icons/fi';
 
 const Projects = () => {
   const projects = [
-    { 
-      title: 'Credit Card Fraud Detection', 
-      description: 'A ML model to detect fraudulent transaction using classification algorithms and data preprocessing techniques', 
-      tech: ['Python', 'Scikit-learn'], 
-      github: 'https://github.com/yashikagupta-ai/Fraud-Detection.git'
+    {
+      title: 'Privacy Policy Grade (GEN AI Project)',
+      description: 'An AI-powered web application that analyzes and grades privacy policies using NLP techniques and LLM-assisted evaluation. The platform assesses readability, legal complexity, dark patterns, and transparency while providing automated insights and policy comparisons.',
+      tech: ['Python', 'Flask', 'Gemini API', 'BeautifulSoup', 'Selenium', 'SQLAlchemy', 'SQLite', 'HTML/CSS', 'JavaScript'],
+      github: 'https://github.com/yashikagupta-ai/privacy-policy-grader-main.git'
     },
+    {
+      title: 'MeshNet (Distributed Systems)',
+      desciption: 'An offline-first emergency communication platform that enables devices to share alerts, safe routes, and aid information without internet or cellular networks using QR-based relay and peer-to-peer communication.',
+      tech: ['React', 'Vite', 'Tailwind CSS','Capacitor','Workbox','Google Nearby Connections API', 'QR Code Technologies', 'Local Storage'],
+      github: 'https://github.com/yashikagupta-ai/MeshNet-main.git'
+    },
+    {
+      title: 'Point based Loaning and Lending (Software Engineering and Full Stack)',
+      description: 'Developed a full-stack web application that allows users to lend and borrow items through a points-based economy, featuring authentication, trust scoring, real-time chat, and item management.',
+      tech: ['React', 'TypeScript', 'Tailwind CSS', 'Node.js', 'Express.js', 'MongoDB', 'Socket.io'],
+      github: 'https://github.com/yashikagupta-ai/SE_Point_based_Loaning_and_leanding.git'
+    },
+    {
+      title: 'digital-stress-contagion ( Agent-Based Modeling and Data Science)',
+      description: 'Developed an agent-based simulation to study how digital stress spreads through social networks, analyzing the impact of user behavior, peer influence, and intervention strategies on overall network well-being.',
+      tech: ['Pytorch', 'Scikit-learn', 'NetworkX','K-Means Clustering','Agent-Based Modeling', 'Data Analysis'],
+    },
+    {
+      title: 'Predicting Human Annotator Disagreement (Deep Neural Network',
+      description: 'Developed a deep learning model to predict human disagreement in image classification tasks by learning full label distributions instead of single-class predictions, enabling better uncertainty estimation and model interpretability.',
+      tech: ['Pytorch', 'Python', 'ResNet-18','Grad-Cam','Deep Learning', 'Computer Vison'],
+      github: 'https://github.com/yashikagupta-ai/Predicting-Human-Annotator-Disagreement.git'
+    },
+    
     { 
-      title: 'Hybrid Predictive Maintenance', 
+      title: 'Hybrid Predictive Maintenance (Machine Learning) ', 
       description: 'The project implements a hybrid predictive maintenance system using the NASA CMAPSS turbofan engine dataset. The goal is to analyze engine sensor data to detect degradation stages, estimate remaining operational life, and generate maintenance risk alerts', 
       tech: ['Python', 'Pandas', 'Matplotlib'], 
       github: 'https://github.com/yashikagupta-ai/ML-project-1-hybrid-.git'
     },
-    { 
-      title: 'Point-Based Item Loaning and Lending System', 
-      description: 'Developing the backend for a point-based peer-to-peer lending platform with risk scoring, transaction management, and gamification features', 
-      tech: ['JavaScript', 'FastAPI', 'JWT Tokens'], 
-      github: 'https://github.com/yashikagupta-ai/SE_Point_based_Loaning_and_leanding.git'
+    {
+      title: 'Credit Card Fraud Detection (Machine Learning)', 
+      description: 'A ML model to detect fraudulent transaction using classification algorithms and data preprocessing techniques', 
+      tech: ['Python', 'Scikit-learn'], 
+      github: 'https://github.com/yashikagupta-ai/Fraud-Detection.git'
+    
     },
+    
   ];
 
   return (
